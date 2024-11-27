77c75d8328c88e3d3002274f39adcc7329e7beff
zhengzihui <zhengzihui@changzhi.top>
2024-11-27 14:09:48 +0800

    buidl zzh

--- configs/configs/zzh/deps.mk
+++ configs/configs/zzh/deps.mk
@@ -1,4 +1,4 @@
-build/ffmpeg-$(FFMPEG_VERSION)/build-base-all/ffbuild/config.mak: build/inst/base/lib/pkgconfig/opus.pc
-build/ffmpeg-$(FFMPEG_VERSION)/build-thr-all/ffbuild/config.mak: build/inst/thr/lib/pkgconfig/opus.pc
-build/ffmpeg-$(FFMPEG_VERSION)/build-base-all/ffbuild/config.mak: build/inst/base/lib/pkgconfig/openh264.pc
-build/ffmpeg-$(FFMPEG_VERSION)/build-thr-all/ffbuild/config.mak: build/inst/thr/lib/pkgconfig/openh264.pc
+build/ffmpeg-$(FFMPEG_VERSION)/build-base-zzh/ffbuild/config.mak: build/inst/base/lib/pkgconfig/opus.pc
+build/ffmpeg-$(FFMPEG_VERSION)/build-thr-zzh/ffbuild/config.mak: build/inst/thr/lib/pkgconfig/opus.pc
+build/ffmpeg-$(FFMPEG_VERSION)/build-base-zzh/ffbuild/config.mak: build/inst/base/lib/pkgconfig/openh264.pc
+build/ffmpeg-$(FFMPEG_VERSION)/build-thr-zzh/ffbuild/config.mak: build/inst/thr/lib/pkgconfig/openh264.pc
